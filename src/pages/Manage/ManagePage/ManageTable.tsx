484e9061814d68f23b6da9e9fc9e52cf0a318787
Saran Kunvijit <lhong2564@gmail.com>
2025-11-15 20:13:20 +0700

    สร้างโต๊ะและลบโต๊ะที่สร้าง

--- src/pages/Manage/ManagePage/ManageTable.tsx
+++ src/pages/Manage/ManagePage/ManageTable.tsx
@@ -1,12 +1,33 @@
 
+import { useState } from 'react'
 import '../ManagePage/ManagePageCss/ManageTable.css'
 function ManageTable() {
+
+  const [tableTotal, setTableTotal] = useState(0)
+  const tables = Array.from({length: tableTotal},(_,i) =>i+1)
+  const handleremoveLastTable = () => {
+    setTableTotal(prev =>(prev > 0? prev-1:0))
+  }
+
   return (
     <div>
       <div className="table-text">
         <div className="all-table">
           <h1>จำนวนโต๊ะทั้งหมด</h1>
-          <p>100 Table</p>
+          <p>{tableTotal} Table</p>
+          
+        </div>
+        
+        <button onClick={() => setTableTotal(prev =>prev+1)} className='btn-create'>สร้างโต๊ะ</button>
+        <button onClick={handleremoveLastTable}>ลบโต๊ะ</button>
+        <div className="btn-table">
+          {tables.map((id) => (
+            <button
+            key={id}
+            className='table-id'
+            onClick={() => console.log(`โต๊ะ ${id}`)}
+            >{id}</button>
+          ))}
         </div>
 
       </div>
