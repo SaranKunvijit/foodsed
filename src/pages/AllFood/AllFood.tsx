566c05a6c47272e2c7bc8442d0bc117850ca64a2
Saran Kunvijit <lhong2564@gmail.com>
2025-11-27 17:43:26 +0700

    Update Menu 2

--- src/pages/AllFood/AllFood.tsx
+++ src/pages/AllFood/AllFood.tsx
@@ -7,12 +7,24 @@ import { ShoppingCart } from 'lucide-react';
 
 
 function AllFood() {
-  const [foods, setFoods] = useState(foodsData);
+  const [foods] = useState(foodsData);
+  const [selectTypeFoods, setSelectTypeFoods] = useState('ทั้งหมด');
+
+  const allType = ['ทั้งหมด',...new Set(foodsData.map(item => item.type))]
+  const fillterType = selectTypeFoods === 'ทั้งหมด' ? foods : foods.filter(item => item.type === selectTypeFoods);
+
+
   return (
     <div>
       <div className="containers">
+        <div className="food-types">
+          {allType.map((type) => (
+            <button key={type} className={selectTypeFoods === type? 'active-type' :''} 
+            onClick={() => setSelectTypeFoods(type)}>{type}</button>
+          ))}
+        </div>
       <div className="header-card">
-          {foods.map((menu) => (
+          {fillterType.map((menu) => (
             <div className="rec-card" key={menu.id}>
               <img src={menu.image} alt="" />
 
